e0043250dd5b38f7fa953dab761623a12e8c1fe4
MIRZOBEKSHODIYEV <mirzobekmirzobekshodiyev@gmail.com>
2025-02-06 18:56:22 +0500

    lesson2

--- lesson-2/homework/string_1.py
+++ lesson-2/homework/string_1.py
@@ -1,3 +1,7 @@
-name=str(input("What is your name: "))
-age=int(input("how old are you: "))
-print(f"hello {name} you born {2024-age} year")
+# name=str(input("What is your name: "))
+# age=int(input("how old are you: "))
+# print(f"hello {name} you born {2025-age} year")
+
+user_input = input("Enter a string: ")
+no_spaces = user_input.replace("isming", "otiyni")
+print("String without spaces:", no_spaces)
---
+++ lesson-2/homework/task.py
@@ -0,0 +1,11 @@
+# a=float(input("sonni kirit"))
+# print(a)
+
+a=float(input("a soni kirit: "))
+b=float(input("b soni kirit: "))
+c=float(input("c soni kirit: "))
+
+katta=max(a, b, c)
+kichik=min(a, b, c)
+print(f'katta son={katta}')
+print(f' kichik son={kichik}')
